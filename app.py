3736c5585c581af339c834acc3c37ea2142863d4
scirop <swarupsahoo@icloud.com>
2019-10-03 23:37:55 -0500

    added 404

--- app.py
+++ app.py
@@ -6,5 +6,11 @@ app = Flask(__name__)
 def pong():
     return jsonify({'data':'pong'})
 
+    
+@app.errorhandler(404)
+def page_not_found(e):
+    return 'Use /ping endpoint', 404
+
+
 if __name__=='__main__':
     app.run()
